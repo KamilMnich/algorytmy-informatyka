5c44a302852ec64df72ec728deb779b357b0ae1d
mnichkamil <kamimnichwp.pl@gmail.com>
2025-03-20 11:58:50 +0100

    schemat hornera WIP

---
+++ Schemat hornera/schemat hornera.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int horner(int wsp[]; int st; int x){
+	
+}
+
+int main() {
+	int stopien;
+	int argument;
+		
+	cout << "Podaj stopien wielomianu: ";
+	cin >> stopien;
+	int wspolczynnik[stopien+1];
+	
+	for(int i = 0; i < stopien; i++){
+		int wspolczynnik[i];
+	}
+	cout << "Podaj argument wielomianu: ";
+	cin >> argument;
+	
+}
