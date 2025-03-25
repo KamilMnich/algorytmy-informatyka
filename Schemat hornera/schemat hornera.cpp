3f4d366f57cd769f43d97e8bfc642c4356044923
KamilMnich <kamimnichwp.pl@gmail.com>
2025-03-25 17:11:01 +0100

    gotowy schemat hornera

--- Schemat hornera/schemat hornera.cpp
+++ Schemat hornera/schemat hornera.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
+
 using namespace std;
 
-int horner(int wsp[]; int st; int x){
-	
+int horner(int wsp[], int st, int x) {
+    if (st == 0)
+        return wsp[0];
+    return x * horner(wsp, st - 1, x) + wsp[st]; // K3
 }
 
 int main() {
-	int stopien;
-	int argument;
-		
-	cout << "Podaj stopien wielomianu: ";
-	cin >> stopien;
-	int wspolczynnik[stopien+1];
-	
-	for(int i = 0; i < stopien; i++){
-		int wspolczynnik[i];
-	}
-	cout << "Podaj argument wielomianu: ";
-	cin >> argument;
-	
+    int stopien, x;
+
+    cout << "Podaj stopien wielomianu: ";
+    cin >> stopien;
+
+    int* wspolczynnik = new int[stopien + 1];
+
+    cout << "Podaj " << stopien + 1 << " wspolczynnikow wielomianu od najwyzszej potegi do wolnego wyrazu:\n";
+    for (int i = 0; i <= stopien; i++) {
+        cin >> wspolczynnik[i];
+    }
+
+    cout << "Podaj argument x: ";
+    cin >> x;
+
+    int wynik = horner(wspolczynnik, stopien, x);
+    cout << "Wartosc wielomianu wynosi: " << wynik << endl;
+
+    delete[] wspolczynnik;
+
+    return 0;
 }
