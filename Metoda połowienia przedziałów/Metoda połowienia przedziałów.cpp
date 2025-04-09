9a11fa39d04d2f3e915a9448a857e11bea23e85e
KamilMnich <99813772+KamilMnich@users.noreply.github.com>
2025-04-09 12:19:25 +0200

    Update Metoda połowienia przedziałów.cpp

--- Metoda połowienia przedziałów/Metoda połowienia przedziałów.cpp
+++ Metoda połowienia przedziałów/Metoda połowienia przedziałów.cpp
@@ -1,38 +1,39 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-float f(int x) {
-	return x*(x*(x-3)+2)-6;
+double f(double x) {
+    return x * (x * (x - 3) + 2) - 6;
+}
+
+double bisekcja(double a, double b, double epsilon) {
+    if (f(a) == 0.0)
+        return a;
+    if (f(b) == 0.0)
+        return b;
+
+    double srodek;
+    while ((b - a) > epsilon) {
+        srodek = (a + b) / 2;
+        if (f(srodek) == 0.0)
+            return srodek;
+        if (f(a) * f(srodek) < 0)
+            b = srodek;
+        else
+            a = srodek;
+    }
+    return (a + b) / 2;
 }
 
 int main() {
-	float a = -10;
-	float b = 15;
-	float epsilon = 0.00001;
-	
-	if(f(a) == 0.0) {
-		return a;
-	}
-	
-	if(f(b) == 0.0) {
-		return b;
-	}
-	
-	while(b-a > epsilon) {
-		float srodek = (a+b)/2;
-		
-		if(f(srodek) == 0) {
-			cout << "Znalezione miejsce zerowe wynosi: " << srodek;
-			return 0;
-		}
-		
-		if(f(a)*f(srodek)<0){
-			b = srodek;
-		}else{
-			a = srodek;
-		}
-	}
-	float win = (a+b)/2;
-	
-	cout << "Znalezione miejsce zerowe wynosi: " << win;
-}  
+    double a = -10;
+    double b = 10;
+    double epsilon = 0.00001;
+
+    double miejsce_zerowe = bisekcja(a, b, epsilon);
+    cout << "Znalezione miejsce zerowe wynosi: " << fixed;
+    cout.precision(5);
+    cout << miejsce_zerowe << endl;
+
+    return 0;
+}
